054e2728baa51debc4274d34c5395218edcb37f5
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-03-26 15:00:35 +0900

    更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+  
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)  
     render json: @posts
   end
 
-  def show
-    @post = Post.find(params[:id])
+    def show
     render json: @post
   end
 
   def create
    @post =  Post.new(post_params)
-
    if @post.save
     render json: @post, status: :created
    else 
@@ -20,7 +20,6 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post =  Post.find(params[:id])
     if @post.update(post_params)
      render json: @post
     else 
@@ -29,12 +28,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post =  Post.find(params[:id])
-
-    @post.destroy
+    @post.destroy 
   end
   
   private
+
+  def set_post 
+    @post = Post.find(params[:id])
+  end
+
   
   def post_params
   params.require(:post).permit(:title, :content)
